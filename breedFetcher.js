0b462c500a2a21a69d78c43c8e56aad9f27af71f
JarrettK <jkirck@hotmaill.com>
2022-04-14 17:34:05 -0400

    updates

---
+++ breedFetcher.js
@@ -0,0 +1,28 @@
+const request = require ("request");
+
+const fetcher = function(breedName) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+request(url, (error, response,body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const data = JSON.parse(body);
+  if (data[0]) {
+    console.log(data[0].description);
+  } else {
+    console.log("breed not found");
+  }
+});
+};
+
+//API Key '89f438d3-c889-4e90-aee3-ead8f0ee00eb'
+/////
+fetchBreedDescription('Siberian', (error, description) => {
+
+});
+
+/////
+const fetchBreedDescription = function(breedName, callback) {
+
+};
